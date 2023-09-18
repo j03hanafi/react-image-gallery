335f7433d8e53ba83a4e1d781e2d5b4b1c6e0371
j03hanafi <namoapak14@gmail.com>
2023-09-18 17:01:28 +0700

    add components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,26 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+import ImageCard from "@/components/ImageCard";
+import ImageSearch from "@/components/ImageSearch";
 
 export default function Home() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.NEXT_PUBLIC_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`,
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
     <>
       <Head>
@@ -9,44 +29,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-          <img
-            src="https://source.unsplash.com/random"
-            alt=""
-            className="w-full"
-          />
-          <div className="px-6 py-4">
-            <div className="font-bold text-purple-500 text-xl mb-2">
-              Photo by John Doe
-            </div>
-            <ul>
-              <li>
-                <strong>Views: </strong>
-                4000
-              </li>
-              <li>
-                <strong>Downloads: </strong>
-                300
-              </li>
-              <li>
-                <strong>Likes: </strong>
-                400
-              </li>
-            </ul>
+      <main className="container mx-auto">
+        <ImageSearch searchText={(text) => setTerm(text)} />
+        {!isLoading && images.length === 0 && (
+          <h1 className="text-center text-6xl mx-auto mt-32">
+            No images found
+          </h1>
+        )}
+        {isLoading ? (
+          <h1 className="text-center text-6xl mx-auto mt-32">Loading...</h1>
+        ) : (
+          <div className="grid grid-cols-3 gap-4 justify-items-center">
+            {images.map((image) => (
+              <ImageCard key={image.id} image={image} />
+            ))}
           </div>
-          <div className="px-6 py-4">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              #tag1
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              #tag2
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              #tag3
-            </span>
-          </div>
-        </div>
+        )}
       </main>
     </>
   );
